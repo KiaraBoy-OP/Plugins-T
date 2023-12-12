61f92ccf8001eef69daa91db575a1d035d4409f3
Zaunka StripeX <109966663+KiaraBoy-OP@users.noreply.github.com>
2023-12-12 23:47:53 +0530

    Delete strings directory

--- strings/__init__.py
+++
@@ -1,6 +0,0 @@
-from TelethonKiara.strings.devs import *
-from TelethonKiara.strings.fun_str import *
-from TelethonKiara.strings.meme_str import *
-from TelethonKiara.strings.others import *
-from TelethonKiara.strings.quotes import *
-from TelethonKiara.strings.rands import *
