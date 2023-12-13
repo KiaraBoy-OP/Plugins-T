2b2d7325d9863c7cd40bb2b6dc3c36380af7e3e0
Zaunka StripeX <109966663+KiaraBoy-OP@users.noreply.github.com>
2023-12-13 13:05:52 +0530

    Update logos.py

--- TelethonKiara/plugins/logos.py
+++ TelethonKiara/plugins/logos.py
@@ -4,13 +4,13 @@ import random
 import requests
 
 from PIL import Image, ImageDraw, ImageFont
-from TelethonHell.plugins import *
+from TelethonKiara.plugins import *
 
 
-@hell_cmd(pattern="logo([\s\S]*)")
+@kiara_cmd(pattern="logo([\s\S]*)")
 async def logo(event):
-    hell = await eor(event, "`Processing.....`")
-    _, _, hell_mention = await client_id(event)
+    kiara = await eor(event, "`Processing.....`")
+    _, _, kiara_mention = await client_id(event)
     text = event.text
     lists = text.split(" ", 1)
     if len(lists) == 1:
@@ -25,13 +25,13 @@ async def logo(event):
     reply = await event.get_reply_message()
     if reply and reply.media and reply.media.photo:
         await reply.download_media("temp_bg.jpg")
-        await hell.edit("__Downloaded replied photo... starting to make logo__")
+        await kiara.edit("__Downloaded replied photo... starting to make logo__")
     else:
         _unsp = await unsplash(_type, 1)
         _bg = requests.get(_unsp[0])
         with open("temp_bg.jpg", "wb") as file:
             file.write(_bg.content)
-        await hell.edit(f"__Downloaded__ `{_type}` __background... starting to make logo__")
+        await kiara.edit(f"__Downloaded__ `{_type}` __background... starting to make logo__")
     img = Image.open("temp_bg.jpg")
     img.resize((5000, 5000)).save("logo_bg.jpg")
     os.remove("temp_bg.jpg")
@@ -69,7 +69,7 @@ async def logo(event):
 
 
 CmdHelp("logos").add_command(
-    "logo", "-{type} {logo text}", "Makes a logo with the given text. If replied to a picture makes logo on that else gets random BG.", f"logo Hellbot \n{hl}logo-car HellBot \n{hl}logo-anime HellBot \netc..."
+    "logo", "-{type} {logo text}", "Makes a logo with the given text. If replied to a picture makes logo on that else gets random BG.", f"logo Kiarabot \n{hl}logo-car KiaraBot \n{hl}logo-anime KiaraBot \netc..."
 ).add_info(
     "Logo Maker."
 ).add_warning(
